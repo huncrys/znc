ed3569d8debc30faff303683bf560926a0f36bd5
Kristof Bach <crys@crys.hu>
2021-03-27 14:59:38 +0100

    Build Docker image with GitLab CI

---
+++ docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ "$(printf '%s' "$1" | cut -c 1)" != '-' ]; then
+    exec "$@"
+fi
+
+: "${DATADIR:="/znc-data"}"
+
+if [ "$(id -u)" = '0' ]; then
+    chown -R znc:znc "$DATADIR" || exit 1
+    chmod 700 "$DATADIR" || exit 2
+    exec su-exec znc:znc /entrypoint.sh "$@"
+fi
+
+{ /usr/sbin/oidentd -i -P "$(route | awk '/^default\s+/ { print $2 }')" > "$DATADIR/.oidentd.log" 2>&1 & } || exit 33
+exec znc --foreground --datadir "$DATADIR" "$@"
